47cf2417aa8118888e7c94aed1351b4f7324e961
Marya <maryammuchai@gmail.com>
2021-05-29 11:33:27 +0300

    added the login view function

--- reviews/views.py
+++ reviews/views.py
@@ -1,3 +1,18 @@
 from django.shortcuts import render
+import datetime as dt
+from .models import Profile, Project, Vote
+from django.contrib.auth.decorators import login_required
 
 # Create your views here.
+@login_required(login_url='/accounts/login/')
+def index(request):
+    title = 'Awwards'
+    date = dt.date.today()
+    projects = Project.display_all_projects()
+    high_votes = None
+    if len(projects)>=1:
+        votes = Vote.get_project_votes
+        high_votes = votes[:3]
+        
+    return render(request, 'index.html', {'date': date, 'title': title, "projects":projects,})
+
